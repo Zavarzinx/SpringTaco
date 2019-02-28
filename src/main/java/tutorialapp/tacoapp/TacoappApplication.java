0e56498dfe1c16ca1b5ec2f59ccec6c537b1dc63
unknown <nazgaard@gmail.com>
2019-02-28 14:29:15 +0400

    Initial commit

---
+++ src/main/java/tutorialapp/tacoapp/TacoappApplication.java
@@ -0,0 +1,13 @@
+package tutorialapp.tacoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tutorialapp/tacoapp/domain/Ingredient.java
@@ -0,0 +1,14 @@
+package tutorialapp.tacoapp.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
